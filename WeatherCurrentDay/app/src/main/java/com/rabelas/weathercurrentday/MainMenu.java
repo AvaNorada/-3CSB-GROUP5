f2afad6f6d44bd52a10825e8b65c0c373c0624f3
RichardWong1020 <richard.wong.iics@ust.edu.ph>
2020-12-03 19:52:00 +0800

    NavBar in progress

--- WeatherCurrentDay/app/src/main/java/com/rabelas/weathercurrentday/MainMenu.java
+++ WeatherCurrentDay/app/src/main/java/com/rabelas/weathercurrentday/MainMenu.java
@@ -1,22 +1,25 @@
 package com.rabelas.weathercurrentday;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.lang.reflect.Array;
+import com.google.android.material.navigation.NavigationView;
+
 
 public class MainMenu extends AppCompatActivity implements AdapterView.OnItemClickListener {
     Toolbar toolbar;
     ConstraintLayout cl;
-    //String[] mainMenu;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +29,14 @@ public class MainMenu extends AppCompatActivity implements AdapterView.OnItemCli
         ListView listView=findViewById(R.id.listView);
         listView.setAdapter(new MainMenuAdapter(this));
 
-        //ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.menu_row, R.id.textView, mainMenu);
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.nav_view);
 
-        //listView.setAdapter(adapter);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+
         listView.setOnItemClickListener(this);
 
         cl = findViewById(R.id.cLayout);
