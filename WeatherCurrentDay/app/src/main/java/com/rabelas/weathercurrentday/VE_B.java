3e9e90844fd05497f7cc86111534df8bb16b5bc8
Ava Norada <70427233+AvaNorada@users.noreply.github.com>
2020-12-03 20:07:56 +0800

    Revert "Norada"

--- WeatherCurrentDay/app/src/main/java/com/rabelas/weathercurrentday/VE_B.java
+++ WeatherCurrentDay/app/src/main/java/com/rabelas/weathercurrentday/VE_B.java
@@ -5,15 +5,6 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
-import android.widget.TextView;
-
-import com.rabelas.weathercurrentday.models.WebpageData;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
 
 public class VE_B extends AppCompatActivity {
     ConstraintLayout cl;
@@ -27,38 +18,5 @@ public class VE_B extends AppCompatActivity {
         toolbar = findViewById(R.id.tool_bar);
         toolbar.setTitle("BEFORE Volcanic Eruption");
         setSupportActionBar(toolbar);
-        final TextView bodyText= findViewById(R.id.webBody);
-        //final TextView titleText= findViewById(R.id.webTitle);
-
-        new Thread(new Runnable() {
-
-            WebpageData data = new WebpageData();
-            @Override
-            public void run() {
-
-
-                try {
-
-                    Document Vdoc =  Jsoup.connect("https://coins.ph/blog/what-to-do-volcanic-eruption-tips/").get();
-
-                    Elements VText = Vdoc.select("div.mk-single-content.clearfix");
-
-                    data.setPageBody(VText.text());
-
-                    data.extractText("EXPLOSION","DURING");//this is dependent on the line above,
-
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {//Set to textview thru WebpageData object
-
-                        bodyText.setText(data.getPageBody());
-                    }
-                });
-            }
-        }).start();
     }
 }
